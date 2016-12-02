edb8a28bfa7a9f0ead489cda0c44c41f4b50d462
dlabhesh30 <dlabhesh@gmail.com>
2016-12-02 02:17:49 -0800

    commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var Uber = require('node-uber');
+var path = require("path");
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var uber = new Uber({
+    client_id: 'dLLuaHaTUPqusPd5oDsk4dNxXLAq7P9y',
+    client_secret: 'RLFm-on4Eyg7QKtQ1ne3pIEpLVi4FCqrCSAwzNDe',
+    server_token: 'DvXtkPFZjeub-qIDGqE477hh_PvzC7veCDWwoiza',
+    redirect_uri: 'http://localhost:5000/api/callback',
+    name: 'myapp3094',
+    language: 'en_US', // optional, defaults to en_US
+    sandbox: true      // optional, defaults to false
+});
+
+app.get('/api/callback', function(request, response) {
+    uber.authorization({
+        authorization_code: request.query.code
+    }, function(err, access_token, refresh_token) {
+        if (err) {
+            console.error(err);
+        } else {
+            // store the user id and associated access token
+            // redirect the user back to your actual app
+            response.redirect('/success');
+        }
+    });
+});
+
+app.get("/success", function (req, res) {
+
+    res.sendFile(path.join(__dirname+"/views/index.html"));
+});
+
+app.post("/results.html", function (req, res) {
+    // uber.estimates.getPriceForRoute(37.338208, -121.886329, 37.3352, -121.8811, function (err, response) {
+    //     console.log(response);
+    // });
+    console.log(req.body);
+    var addr=req.body;
+    geocodeAddress(addr);
+    res.sendFile(path.join(__dirname+"/views/results.html"));
+});
+
+app.get('/', function(request, response) {
+    var url = uber.getAuthorizeUrl(['history','profile', 'request', 'places', 'all_trips']);
+    response.redirect(url);
+});
+
+app.listen(5000, function () {
+    console.log("Path Finder started at port 5000");
+});
+
+function geocodeAddress(addr) {    
+        var geocoder = new google.maps.Geocoder();    
+        geocoder.geocode({'address': addr[0]}, function(results, status) {
+          if (status === 'OK') {
+            lat=results[0].geometry.location.lat();
+            lon=results[0].geometry.location.lon();
+            console.log(lat,lon);
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+
+    }
---
+++ public/js/script.js
@@ -0,0 +1,102 @@
+ var wayPts=[];
+ var latLngPlaces=[];
+ var place_ids=[];
+ var address=[];
+ var boxCount = 1;
+ var map;
+ var origin_id;
+ var dest_id;
+ var lastDest;
+ var lastBoxNo;
+function initMap() {       
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 6,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
+        document.getElementById('submit').addEventListener('click', function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        });                  
+      }
+//AIzaSyDFGOGmf-1DTREZJoP9Si319k1hnUWZXTw google map key
+function getPlaces(pac_input){
+   var input = document.getElementById(pac_input);
+        var options = {
+            types : ['address']
+        };
+        var latitude;
+        var longitude;
+        autocomplete = new google.maps.places.Autocomplete(input, options);
+        autocomplete.addListener('place_changed', function(){
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+              window.alert("Autocomplete's returned place contains no geometry");
+              return;
+            }
+            
+            latitude = place.geometry.location.lat();
+            longitude = place.geometry.location.lng();
+            var presentBox=pac_input.substring(9,10);
+            latLngPlaces.splice(parseInt(presentBox),1,{latitude,longitude});
+            if(parseInt(presentBox)==0){
+              place_ids.splice(0,0,place.place_id);
+              address.splice(parseInt(presentBox),1,document.getElementById(pac_input).value);
+            }
+            else {            
+            
+            place_ids.splice(parseInt(presentBox),1,place.place_id);
+            address.splice(parseInt(presentBox),1,document.getElementById(pac_input).value);             
+            if(lastBoxNo==(parseInt(presentBox)-1) || parseInt(presentBox)==1){              
+              if(parseInt(presentBox)!=1)
+              wayPts.splice(lastBoxNo,1,{location:lastDest, stopover: true});
+              lastDest=document.getElementById(pac_input).value;
+              lastBoxNo=parseInt(presentBox);
+            }
+            console.log("Lat - " + latitude + " Long - " + longitude +" boxno -"+presentBox );
+            console.log(wayPts);                      
+            console.log(latLngPlaces);
+            
+          }
+        });
+}
+
+//This function calculates the route and places markers on the recieved input... This is final function dont change anything in this..  
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {                      
+        directionsService.route({
+          origin: {'placeId': place_ids[0]},
+          destination: {'placeId': place_ids[boxCount]},
+          waypoints: wayPts,
+          optimizeWaypoints: true,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);            
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+
+$(function () {  
+    $(".add-button").bind("click", function () {
+      boxCount++;
+
+        var div = $("<div />");
+        div.html('<input type="text" class="form-control address" id="pac-input'+ boxCount +'" onfocus="getPlaces(this.id)" placeholder="New Destination" />&nbsp;' +
+            '<input type="button" value="X" class="btn btn-primary text-center remove" id="remove'+boxCount +'" />');
+        $(".address-boxes").append(div);
+
+    });    
+    $("body").on("click", ".remove", function () {     
+      boxCount--;            
+        $(this).closest("div").remove();
+    });
+
+    $(".result-button").click(function(){
+      console.log("Hey");
+      $.get("results");
+    });
+
+});
