b33892da6013b90f9dbadc3d00812c41ac6ed5e9
shauryam <shaurya_mittal@ymail.com>
2016-12-07 06:04:42 -0800

    Add files via upload

---
+++ public/js/results_script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $("ul li").click(function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    var rows = $("#rows").html();
+    console.log(rows);
+    var total = 0;
+    for(var i=1; i<=rows; i++){
+    	 var uberPriceID =  "#uber" + i;
+    	 var lyftPriceID =  "#lyft" + i;
+    	 var resultPriceID =  "#result" + i;
+    	 console.log(uberPriceID);
+    	 var uberPrice = $(uberPriceID).html();
+    	 var lyftPrice = $(lyftPriceID).html();
+    	 console.log(uberPrice);
+    	 console.log(lyftPrice);
+    	 if(uberPrice<lyftPrice){
+    	 	$(resultPriceID).html(uberPrice + '<img src="uber.png">');
+    	 	total = parseFloat(total) + parseFloat(uberPrice);
+    	 }
+    	 else{
+    	 	$(resultPriceID).html(lyftPrice + '<img src="lyft.png">');
+    	 	total = total + parseFloat(lyftPrice);
+    	 }
+
+    }
+    $("#total").html(total);
+    $("#rows").hide();
+
+});
